3dc949355e9baf89ccc228176d5f59055f8d532f
zilong.zhou <zzl81cn@163.com>
2016-06-29 17:09:31 +0800

    modify inherit

---
+++ src/com/myjava/inherit/Bird.java
@@ -0,0 +1,10 @@
+package com.myjava.inherit;
+
+/**
+ * Created by zhouzilong on 2016/6/29.
+ */
+public class Bird {
+    public void fly(){
+        System.out.print("oooo");
+    }
+}
---
+++ src/com/myjava/inherit/Ostrich.java
@@ -0,0 +1,15 @@
+package com.myjava.inherit;
+
+/**
+ * Created by zhouzilong on 2016/6/29.
+ */
+public class Ostrich extends Bird {
+    @Override
+    public void fly(){
+        System.out.println("bbb");
+    }
+    public static void main(String args[]){
+        Ostrich os = new Ostrich();
+        os.fly();
+    }
+}
