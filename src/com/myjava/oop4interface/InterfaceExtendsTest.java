e85d2222eea18393e1a9e2a706c98d9fc1e12959
zilong.zhou <zzl81cn@163.com>
2016-07-01 17:24:52 +0800

    modify OOP's inherit.

---
+++ src/com/myjava/oop4interface/InterfaceExtendsTest.java
@@ -0,0 +1,24 @@
+package com.myjava.oop4interface;
+
+/**
+ * Created by zhouzilong on 2016/7/1.
+ */
+interface interfaceA {
+    int PRO_A = 5;
+    void testA();
+}
+interface interfaceB {
+    int PRO_B = 8;
+    void testB();
+}
+interface interfaceC extends interfaceA, interfaceB {
+    int PRO_C = 10;
+    void testC();
+}
+public class InterfaceExtendsTest {
+    public static void main(String[] args) {
+        System.out.println(interfaceC.PRO_A);
+        System.out.println(interfaceC.PRO_B);
+        System.out.println(interfaceC.PRO_C);
+    }
+}
---
+++ src/com/myjava/oop4interface/Output.java
@@ -0,0 +1,10 @@
+package com.myjava.oop4interface;
+
+/**
+ * Created by zhouzilong on 2016/7/1.
+ */
+public interface Output {
+    int MAX_CACHE_LINE = 50;
+    void out();
+    void getData(String msg);
+}
