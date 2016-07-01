1782b10a8c7ed51ab293456f1feb768a3bce08fc
zilong.zhou <zzl81cn@163.com>
2016-07-01 17:56:35 +0800

    modify OOP's inherit.

--- src/com/myjava/oop4interface/Printer.java
+++ src/com/myjava/oop4interface/Printer.java
@@ -8,9 +8,14 @@ interface Product {
 }
 public class Printer implements Output, Product {
     private String [] printData = new String[MAX_CACHE_LINE];
+    private int dataNum = 0;
     @Override
     public void out() {
-
+        while(dataNum > 0){
+            System.out.println("打印机打印：" + printData[0]);
+            // 把作业队列整体前移一位，并将剩下的作业数减1
+            System.arraycopy(printData, 1, printData, 0, --dataNum);
+        }
     }
 
     @Override
