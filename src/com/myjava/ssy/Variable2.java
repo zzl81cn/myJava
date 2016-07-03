28ba222c543430aed35170f0eba2322675be6176
zilong.zhou <zzl81cn@163.com>
2016-07-03 23:25:32 +0800

    add ssy4enum

--- src/com/myjava/ssy/Variable2.java
+++ src/com/myjava/ssy/Variable2.java
@@ -1,18 +1,18 @@
 package com.myjava.ssy;
 
 public class Variable2 {
-public static void main(String []args){
-	//double a=1.2;
-	//System.out.println(a);
-	float ab = (float)1.2;//���д���������
-	System.out.println(ab);
-	float cd = 1.2f;
-	System.out.println(cd);
-	char a = 'B';
-	System.out.println(a);
-	char b = '��';
-	System.out.println(b);
-	boolean c = true;
-	System.out.println(c);
-}
+    public static void main(String []args){
+        //double a=1.2;
+        //System.out.println(a);
+        float ab = (float)1.2;//这行代码编译出错
+        System.out.println(ab);
+        float cd = 1.2f;
+        System.out.println(cd);
+        char a = 'B';
+        System.out.println(a);
+        char b = '张';
+        System.out.println(b);
+        boolean c = true;
+        System.out.println(c);
+    }
 }
