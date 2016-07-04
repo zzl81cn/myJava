2f142a0868c331261f45ac674339856d9c56f930
zilong.zhou <zzl81cn@163.com>
2016-07-04 18:02:12 +0800

    modify OOP's interface

--- src/com/myjava/collection4list/ListTest.java
+++ src/com/myjava/collection4list/ListTest.java
@@ -14,9 +14,18 @@ public class ListTest {
         books.add(new String("Crazy Android"));
         System.out.println(books);
         System.out.println("--------");
+        // Insert into index 2
         books.add(1, new String("Crazy AJAX"));
         for(int i = 0;i < books.size();i++){
             System.out.println(books.get(i));
         }
+        System.out.println("--------");
+        books.remove(2);
+        System.out.println("delete 2nd element: " + books);
+        System.out.println(books.indexOf(new String("Crazy AJAX")));
+        books.set(1, new String("Crazy Java"));
+        System.out.println(books);
+        // 到第三个元素（不含）截取成子集合
+        System.out.println(books.subList(1, 2));
     }
 }
