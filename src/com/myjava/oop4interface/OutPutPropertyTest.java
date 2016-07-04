c71c7e44eab5ebe37328f4ec50d27ec714f99b1a
zilong.zhou <zzl81cn@163.com>
2016-07-04 09:57:08 +0800

    modify OOP's inherit.

---
+++ src/com/myjava/oop4interface/OutPutPropertyTest.java
@@ -0,0 +1,11 @@
+package com.myjava.oop4interface;
+
+/**
+ * Created by zhouzilong on 2016/7/4.
+ * 接口里的Field默认是public static final修饰的，即便另一个类不在同一个包下也可以通过接口来访问接口里的常量Field。
+ */
+public class OutPutPropertyTest {
+    public static void main(String[] args) {
+        System.out.println(Output.MAX_CACHE_LINE);
+    }
+}
--- src/com/myjava/oop4interface/Output.java
+++ src/com/myjava/oop4interface/Output.java
@@ -2,6 +2,7 @@ package com.myjava.oop4interface;
 
 /**
  * Created by zhouzilong on 2016/7/1.
+ * 定义一个接口，里面包含一个常量field:MAX_CACHE_LINE，除此之外还定义了两个方法，一个取得数据，一个输出。
  */
 public interface Output {
     int MAX_CACHE_LINE = 50;
