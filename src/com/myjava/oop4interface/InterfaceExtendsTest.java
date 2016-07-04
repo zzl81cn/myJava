dd323a33a829ca09c3cb36bdaa920c1093ae26e8
zilong.zhou <zzl81cn@163.com>
2016-07-04 09:38:08 +0800

    modify OOP's inherit.

--- src/com/myjava/oop4interface/InterfaceExtendsTest.java
+++ src/com/myjava/oop4interface/InterfaceExtendsTest.java
@@ -3,6 +3,7 @@ package com.myjava.oop4interface;
 /**
  * Created by zhouzilong on 2016/7/1.
  * #lesson 6.6.3
+ * 接口支持多继承，即一个接口可以有多个直接父接口。和类继承类似，子接口扩展某个父接口，将会获得父接口里定义的所有抽象方法、常量Field。
  */
 interface interfaceA {
     int PRO_A = 5;
